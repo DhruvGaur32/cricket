f1bfe7d6d5c8312b41bbd32da9de49d9a7903d73
chrisrex <krishcharniya007@gmail.com>
2024-06-28 06:02:42 +0530

    volleyball updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes,Route } from "react-router-dom"
 import Home from "./components/Home"
+import Volleyball from "./Volleyball"
 function App() {
 
   return (
@@ -7,6 +8,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/volleyball" element={<Volleyball/>}/>
       </Routes>
     </Router>
       
