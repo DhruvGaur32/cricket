27705b769de18eb8ef0029d6d87a4cb338b852bd
captaincoro11 <121259483+captaincoro11@users.noreply.github.com>
2024-06-28 15:23:24 +0530

    Update App.jsx
    UserEndView Route error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/athletics" element={<UserEndView }/>
+        <Route path="/athletics" element={<UserEndView/>}/>
         
         
       </Routes>
